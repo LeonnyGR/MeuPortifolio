7a096e44c53b02b472331bf5e1b7c3964ba4f263
LeonnyGR <leonnyraiz@gmail.com>
2022-01-05 15:34:30 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function verde(){
+    document.getElementById('corpo').style.background= "linear-gradient(68.15deg, #0a2e0d 16.62%, #12a30d 85.61%)";
+}
+
+function roxo(){
+    document.getElementById('corpo').style.background= "linear-gradient(68.15deg, #130612 16.62%, #470e68 85.61%)";
+}
+
